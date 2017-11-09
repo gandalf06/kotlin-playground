4ab240385da2a4170d278bbca8e0ff361eaa32b9
Darlan Oliveira <feitt.c6@gmail.com>
2017-11-09 17:15:39 -0200

    Controle de fluxo When
    Entendendo When do Kotlin.

---
+++ src/CTWhen.kt
@@ -0,0 +1,9 @@
+
+fun main(args: Array<String>){
+    var opcao = 3
+    when(opcao){
+        1 -> println("Café")
+        2 -> println("Suco")
+        else -> println("Selecione sua bebida")
+    }
+}
