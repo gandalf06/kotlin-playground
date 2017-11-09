7446469915c6d62a63f4902d16d14e2bb5ba0eb9
Darlan Oliveira <feitt.c6@gmail.com>
2017-11-09 17:02:39 -0200

    Tipos de variavies kotlin
    Tipos de variavies kotlin

---
+++ src/TiposDeVariavies.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+  
+    //Double
+    var numero = 3.55533434
+    // Float= por F de Float ao final do numero 
+    var numero2: Float = 5.3f
+    //Int
+    var numerointerio:Int = 10
+    //Long - por L de longo ao final do numero
+    var numerolong: Long = 1000000L
+    //Boolean
+    var resultado = true 
+    //var numero1 = "Darlan"
+    
+   	println(resultado)
+}
